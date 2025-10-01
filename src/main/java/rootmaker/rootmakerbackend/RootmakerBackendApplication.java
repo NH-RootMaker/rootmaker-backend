f61bdc809d328d03e7031eafee53c2ff7e1eb2c1
kyoohyunlee <khyhlee@naver.com>
2025-10-01 14:10:10 +0900

    initial commit

---
+++ src/main/java/rootmaker/rootmakerbackend/RootmakerBackendApplication.java
@@ -0,0 +1,13 @@
+package rootmaker.rootmakerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootmakerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootmakerBackendApplication.class, args);
+    }
+
+}
